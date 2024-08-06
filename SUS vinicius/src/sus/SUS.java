2a7ee6bdcd0c38c03ff5623ca40fb5200764db70
Viniciusmagal <162904345+Viniciusmagal@users.noreply.github.com>
2024-08-05 22:43:54 -0300

    Add files via upload

---
+++ SUS vinicius/src/sus/SUS.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sus;
+
+import Telas.Tela1;
+
+/**
+ *
+ * @author Aluno
+ */
+public class SUS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Tela1 ex = new Tela1();
+        ex.setVisible(true);
+                
+    }
+    
+}
